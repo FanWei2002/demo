17a2b27603a5694498a64a02a59e84edcea1b386
admin <2950112814@qq.com>
2022-09-23 14:54:19 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,7 +1,13 @@
 package com.atguigu.git;
 
+import java.util.Scanner;
+
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello git!");
+        Scanner sc =new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
     }
 }
